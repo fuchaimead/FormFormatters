31e5d086c2a28316285f39617173e34fe52a12f7
Mike Hoffert <mike.hoffert@gmail.com>
2018-08-14 07:54:32 -0600

    Date parsed should be epoch

--- src/formatters/date.js
+++ src/formatters/date.js
@@ -8,7 +8,7 @@ export default function({errors, formatted: value, parsed}) {
     let temp = date.parse(parsed);
     if(temp.isValid()) {
       // store parsed value as just the date portion.
-      parsed = temp.format("YYYY-MM-DD");
+      parsed = temp.unix();
       formatted = temp.format("MMM D, YYYY");
     } else {
       errors.push("FormFormatters.dateInvalid");

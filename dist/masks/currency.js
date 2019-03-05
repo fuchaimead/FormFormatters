9d7737cb230e312c200f0efde59b57e36896f6d2
Mike Hoffert <mike.hoffert@jolt.com>
2019-03-05 13:15:14 -0700

    Dist files

--- dist/masks/currency.js
+++ dist/masks/currency.js
@@ -4,7 +4,7 @@ var _lodash = require("lodash");
 
 var CurrencyMask = {
   mask: function mask(value) {
-    return (0, _lodash.isNil)(value) ? null : value.toString().replace(/[^0-9.,]/g, "");
+    return (0, _lodash.isNil)(value) ? null : value.toString().replace(/[^0-9.,-]/g, "");
   }
 };
 

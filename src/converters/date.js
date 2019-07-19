5488f5e592fbccfd319917f1ca19459262250392
FuChai Mead <fuchai.mead@tazworks.com>
2019-07-19 15:40:04 -0600

    remove utc from date converter

--- src/converters/date.js
+++ src/converters/date.js
@@ -3,7 +3,7 @@ import { isNil } from "lodash";
 
 export default function(value) {
   if(!isNil(value) && value !== "") {
-    let temp = Moment.unix(value).utc();
+    let temp = Moment.unix(value);
     if(temp.isValid()) {
       value = temp.clone().format("MMM D, YYYY");
     }
